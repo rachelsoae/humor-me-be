5b29921625df308dfa1c63a28a2db3c42e6b3aa2
Daniel Cochico <dcochico.20@gmail.com>
2023-07-27 20:13:41 -0700

    feat: Add minor adjustment to organization of variables

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
-const cors = require('cors');
 
 app.use(cors());
 app.use(express.json());
